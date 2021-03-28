ade5db7aa95a8bd2f5481d3e4ed84e7e1e1d40da
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-03-28 13:56:46 +0000

    week04

---
+++ Clang/week04.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void){
+    printf("서휘도 20205188\n");
+    char x;
+    int y=1;
+    for(int i=0;i<2;i++){
+        printf("숫자를 입력하세요:");
+        x=getchar();
+        getchar();
+        if(x>=48&&x<=58){
+            x-=48;    
+        }else{
+            x=1;
+        }
+        y*=x;
+    }
+    printf("두수의 곱은 = %d\n",y);
+    
+}
