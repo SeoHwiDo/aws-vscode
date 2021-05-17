3c131eea0763fd6fab23956c1a57b97c43307665
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-05-17 03:49:24 +0000

    week11

---
+++ java/List/PolyList/PolyNode.java
@@ -0,0 +1,13 @@
+package java.List.PolyList;
+
+public class PolyNode {
+    private int coef;
+    private int exp;
+    private PolyNode link;
+    public PolyNode(){
+        coef=0;
+        exp=0;
+        link=null;
+    }
+
+}
---
+++ java/List/PolyList/Polynomial.java
@@ -0,0 +1,23 @@
+package java.List.PolyList;
+
+public class Polynomial {
+    private PolyNode first;
+    private PolyNode last;
+
+    Polynomial(){
+        first=last=null;
+    }
+    public void addTerm(int c, int e){
+        PolyNode p= new PolyNode();
+        while(p.getLink()!=null){
+            if(e>p.getLink().exp){
+
+            }
+        }
+    }
+    public Polynomial polyAdd(Polynomial p) {
+        Polynomial res=new Polynomial();
+    }
+
+    public Polynomial appen
+}
